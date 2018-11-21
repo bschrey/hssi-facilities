41626225e0dc1559140a86ea22c62ca83b6eaae6
Brett Schrey <brettschrey@gmail.com>
2018-11-21 11:38:36 -0600

    cleanup

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
+const createError = require('http-errors');
 const express = require('express');
-const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
 
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.json({"message": "Facilities Server."});
 });
 
 require('./routes/facility-routes.js')(app);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+  	"error": err.status,
+	"message": err.message,
+	"stack": err.stack
+	});
+});
 
 const port = process.env.PORT || 3000;
 
